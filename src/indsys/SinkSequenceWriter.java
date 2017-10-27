de398fb0733ffdb8f0572ac7ed77bf53e22c30d0
tscholy <christina.tschol@students.fhv.at>
2017-10-27 13:00:07 +0200

    test

---
+++ src/indsys/SinkSequenceWriter.java
@@ -0,0 +1,42 @@
+package indsys;
+
+import pmp.filter.Sink;
+import pmp.interfaces.Writeable;
+
+import javax.sound.sampled.Line;
+import java.io.*;
+import java.util.ArrayList;
+
+public class SinkSequenceWriter extends Sink<ArrayList<Sequence>> implements Writeable<ArrayList<Sequence>> {
+
+    public static final String OUTPUTFILE = "Outputfiles\\textWithIndex.txt";
+
+    public SinkSequenceWriter() {
+        super();
+    }
+
+    @Override
+    public void write(ArrayList<Sequence> sequences) throws StreamCorruptedException {
+        Writer sinkWriter = null;
+
+        try {
+            sinkWriter = new BufferedWriter(
+                    new OutputStreamWriter(
+                    new FileOutputStream(OUTPUTFILE), "utf-8"));
+
+            for(Sequence sequence : sequences) {
+                for(String word : sequence.getSequenceWords()) {
+                    sinkWriter.write(word + " ");
+                }
+                sinkWriter.write(sequence.getLineNumber() + System.lineSeparator());
+            }
+        } catch (IOException ex) {
+            // report
+        } finally {
+            try {
+                sinkWriter.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+}
